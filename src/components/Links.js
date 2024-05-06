cf8c12fde6669b77763971c0caa7cc6556cf3ec6
John <jgicheru325@gmail.com>
2024-05-06 11:58:44 +0300

    Props lab

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+function Links(props){
+    return(
+        <div className="links">
+            <h3>Links</h3>
+            <a href={props.github}>{props.github}</a>
+            <br/>
+            <a href={props.linkedin}>{props.linkedin}</a>
+        </div>
+    )
+}
+export default Links;
